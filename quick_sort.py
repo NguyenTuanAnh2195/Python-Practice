9d72bd7ce1c40695529505f5dacb8fa2698ece96
NguyenTuanAnh2195 <anhnt20101995@gmail.com>
2022-06-06 23:14:50 +0700

    Fix up quicksort a bit more for debugging

--- quick_sort.py
+++ quick_sort.py
@@ -2,9 +2,12 @@ import random
 import datetime
 
 
-def get_pivot(arr):
+def get_pivot(arr, l, r):
     random.seed(datetime.datetime.now())
-    return random.choice(range(len(arr)))
+    # right = r + 1 if r < len(arr) else len(arr)
+    return get_median(arr, l, r)
+    return (l + r) // 2
+    return random.choice(range(len(arr[l:right])))
 
 
 def partition(arr, l, r):
@@ -20,26 +23,48 @@ def partition(arr, l, r):
     return after_pivot - 1
 
 
-def merge_sort(arr, left, right):
+def get_median(arr, l, r):
+    if l >= r:
+        return max(arr[l:r])
+
+    temp_arr = arr[l:r]
+    first_val = temp_arr[0]
+    last_val = temp_arr[-1]
+    middle_val = temp_arr[(len(temp_arr) // 2) - 1]
+    selection_pool = [first_val, middle_val, last_val]
+    exclusion_pool = [max(temp_arr), min(temp_arr)]
+    print(selection_pool, exclusion_pool)
+    result_arr = [num for num in selection_pool if num not in exclusion_pool]
+    return result_arr[0]
+    
+
+
+def quick_sort(arr, left, right):
+    # import pdb
+    # pdb.set_trace()
     if left >= right:
         return arr
-    arr[left], arr[right - 1] = arr[right- 1], arr[left]
-    pivot_index = partition(arr, left, right)
-    merge_sort(arr, left, pivot_index)
-    merge_sort(arr, pivot_index + 1, right)
+    # arr[left], arr[right - 1] = arr[right- 1], arr[left]
+    pivot = get_pivot(arr, left, right)
+    if pivot != left:
+        arr[pivot], arr[left] = arr[left], arr[pivot]
+    pivot_index = partition(arr, pivot, right)
+    quick_sort(arr, left, pivot_index)
+    quick_sort(arr, pivot_index + 1, right)
 
     return arr
      
 
 
 
-arr = [12, 3, 4, 10, 1, 15, 8, 6, 0, 13, 11, 2, 5, 7, 9, 14]
+arr = [2, 20, 1, 15, 3, 11, 13, 6, 16, 10, 19, 5, 4, 9, 8, 14, 18, 17, 7, 12]
+arr = [4, 1, 5, 2, 3, 0]
 comparisons = 0
-with open("qs_array.txt", "r") as f:
-    arr = [int(i) for i in f]
+# with open("qs_array.txt", "r") as f:
+#     arr = [int(i) for i in f]
 
 
-merge_sort(arr, 0, len(arr))
-# print(f"Result: {arr}")
+quick_sort(arr, 0, len(arr))
+print(f"Result: {arr}")
 print(f"Comparisons: {comparisons}")
 
