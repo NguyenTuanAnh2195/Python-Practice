f58a2b0aa84056ef44a1c0ae80d638c5c202760e
tuananh <tuan.anh.nguyen02@advesa.com>
2022-07-25 18:21:42 +0700

    Fix get median

--- quick_sort.py
+++ quick_sort.py
@@ -5,7 +5,9 @@ import datetime
 def get_pivot(arr, l, r):
     random.seed(datetime.datetime.now())
     # right = r + 1 if r < len(arr) else len(arr)
-    return get_median(arr, l, r)
+    result = arr.index(get_median(arr, l, r))
+    return result
+
     return (l + r) // 2
     return random.choice(range(len(arr[l:right])))
 
@@ -24,16 +26,21 @@ def partition(arr, l, r):
 
 
 def get_median(arr, l, r):
-    if l >= r:
+    if len(arr[l:r]) <= 2:
         return max(arr[l:r])
-
+    
     temp_arr = arr[l:r]
+
     first_val = temp_arr[0]
     last_val = temp_arr[-1]
-    middle_val = temp_arr[(len(temp_arr) // 2) - 1]
+    if len(temp_arr) % 2 == 0:
+        middle_val = temp_arr[len(temp_arr) // 2]
+    else:
+        middle_val = temp_arr[(len(temp_arr) -1) // 2]
     selection_pool = [first_val, middle_val, last_val]
-    exclusion_pool = [max(temp_arr), min(temp_arr)]
-    print(selection_pool, exclusion_pool)
+    exclusion_pool = [max(selection_pool), min(selection_pool)]
+    if len(set(selection_pool)) <= 2:
+        return max(selection_pool)
     result_arr = [num for num in selection_pool if num not in exclusion_pool]
     return result_arr[0]
     
@@ -46,19 +53,19 @@ def quick_sort(arr, left, right):
         return arr
     # arr[left], arr[right - 1] = arr[right- 1], arr[left]
     pivot = get_pivot(arr, left, right)
+    print(f"Pivot: {pivot}")
     if pivot != left:
         arr[pivot], arr[left] = arr[left], arr[pivot]
     pivot_index = partition(arr, pivot, right)
     quick_sort(arr, left, pivot_index)
     quick_sort(arr, pivot_index + 1, right)
-
     return arr
      
 
 
 
-arr = [2, 20, 1, 15, 3, 11, 13, 6, 16, 10, 19, 5, 4, 9, 8, 14, 18, 17, 7, 12]
-arr = [4, 1, 5, 2, 3, 0]
+# arr = [2, 20, 1, 15, 3, 11, 13, 6, 16, 10, 19, 5, 4, 9, 8, 14, 18, 17, 7, 12]
+arr = [4, 1, 5, 2, 3, 0, 6]
 comparisons = 0
 # with open("qs_array.txt", "r") as f:
 #     arr = [int(i) for i in f]
